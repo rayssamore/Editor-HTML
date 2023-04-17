ac99c0db7f15609f20def2b89a5d7fa03ae5c137
unknown <rayssagm_@hotmail.com>
2023-04-16 21:45:42 -0300

    Versão 1.0.0

---
+++ Menu.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorHTML
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            DrawScreen();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
+
+        }
+        public static void DrawScreen()
+        {
+            GeraLinhaCabFin(30);
+            GeraEstruturaTexto(10, 30);
+            GeraLinhaCabFin(30);
+        }
+        static void GeraLinhaCabFin(int colunas)
+        {
+            Console.Write("+");
+            for (int i = 0; i <= colunas; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+
+        }
+        static void GeraEstruturaTexto(int linhas, int colunas)
+        {
+            for (int lines = 0; lines <= linhas; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= colunas; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+                Console.Write("\n");
+            }
+        }
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("============");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo Arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção: ");
+
+
+        }
+        public static void HandleMenuOption(short option)
+        {
+            switch (option)
+            {
+                case 1: Editor.Show(); break;
+                case 2: Console.Write("View"); break;
+                case 0:
+                    {
+                        Console.Clear();
+                        Environment.Exit(0);
+                        break;
+                    }
+                default: Show(); break;
+
+
+            }
+        }
+    }
+}
